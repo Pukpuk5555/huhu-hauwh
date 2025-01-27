eb846458b05a61a0d8811d7e872a724a8fbcd5f2
Pukkkkk <pukpukmakinggame1111@gmail.com>
2025-01-27 23:20:22 +0700

    IngredientsCooking Script

---
+++ Assets/Scripts/IngradientsCooking.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngradientsCooking : MonoBehaviour
+{
+    protected SpriteRenderer spriteRenderer;
+
+    protected Sprite cookSprite;
+    protected Color rawColor;
+    protected Color cookedColor = new Color(0.6f, 0.3f, 0.2f);
+    protected float cookingTimer = 0f;
+    protected bool isCooking = false;
+
+    protected virtual void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rawColor = spriteRenderer.color;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        OnCooking();
+    }
+
+    public virtual void StartCooking()
+    {
+        if(!isCooking)
+        {
+            isCooking = true;
+            cookingTimer = 0f;
+            Debug.Log("Start Cooking");
+        }
+    }
+
+    public virtual void OnCooking()
+    {
+        
+    }
+}
